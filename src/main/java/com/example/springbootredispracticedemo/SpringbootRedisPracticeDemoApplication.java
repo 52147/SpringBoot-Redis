5a686ccb7b803dade882d6527685d4e9427c2e64
Your Name <debra4117@gmail.com>
2023-02-10 17:48:55 -0500

    first commit

---
+++ src/main/java/com/example/springbootredispracticedemo/SpringbootRedisPracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootredispracticedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisPracticeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisPracticeDemoApplication.class, args);
+    }
+
+}
